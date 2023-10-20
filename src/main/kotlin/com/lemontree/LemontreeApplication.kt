81b3534650e59e4e80cbc8c83e2653fa8dd8c120
yanglet <didcnddnr@skuniv.ac.kr>
2023-10-20 21:22:22 +0900

    프로젝트 세팅

---
+++ src/main/kotlin/com/lemontree/LemontreeApplication.kt
@@ -0,0 +1,11 @@
+package com.lemontree
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LemontreeApplication
+
+fun main(args: Array<String>) {
+	runApplication<LemontreeApplication>(*args)
+}
---
+++ src/test/kotlin/com/lemontree/LemontreeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lemontree
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LemontreeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
